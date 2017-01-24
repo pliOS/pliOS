78dc2318ec56653c75f7fed3a97d194adbd6eea8
SdNssr <SdNssr@users.noreply.github.com>
2017-01-24 13:10:44 +0530

    Fixed a small error

--- build/includes/main.mk
+++ build/includes/main.mk
@@ -9,9 +9,9 @@ all: toolchain
 
 INCLUDE_DIR := ${PLIOS_ROOT}/build/includes
 
-export PATH := ${PLIOS_ROOT}/build/scripts:$PATH
-export PATH := ${PLIOS_OUT}/toolchain/c/bin:$PATH
-export PATH := ${PLIOS_OUT}/toolchain/go/root/bin:$PATH
+export PATH := ${PLIOS_ROOT}/build/scripts:${PATH}
+export PATH := ${PLIOS_OUT}/toolchain/c/bin:${PATH}
+export PATH := ${PLIOS_OUT}/toolchain/go/root/bin:${PATH}
 
 export GOROOT := ${PLIOS_OUT}/toolchain/go/root
 export GOPATH := ${PLIOS_OUT}/toolchain/go/path
